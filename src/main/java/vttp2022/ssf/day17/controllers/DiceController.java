b9901bebb13e8983292d705276c7e113ade57360
daryl-goh <goh.daryl@gmail.com>
2022-08-07 16:36:07 +0800

    added hellocontroller

--- src/main/java/vttp2022/ssf/day17/controllers/DiceController.java
+++ src/main/java/vttp2022/ssf/day17/controllers/DiceController.java
@@ -24,7 +24,6 @@ public class DiceController {
 
         model.addAttribute("dices", rolls);
         return "roll.html";
-
     }
     
 }
--- src/main/java/vttp2022/ssf/day17/controllers/DiceRestController.java
+++ src/main/java/vttp2022/ssf/day17/controllers/DiceRestController.java
@@ -19,7 +19,7 @@ import vttp2022.ssf.day17.services.DiceService;
 
 @RestController
 // produces -> Accept:
-@RequestMapping(path="/roll", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path="/roll")
 public class DiceRestController {
 
     @Autowired
@@ -29,59 +29,60 @@ public class DiceRestController {
     public ResponseEntity<String> getRollAsCSV(
             @RequestParam(name = "dice", defaultValue = "1") Integer count) {
         if ((count < 1) || (count > 10)) {
-            
             String error = "Error: Valid dice count is between 1 and 10. Your count is %d".formatted(count);
             return ResponseEntity
-                    //.status(HttpStatus.BAD_REQUEST)
+                    //.status(HttpStatus.BAD_REQUEST) 
                     .badRequest() //400
                     .body(error);
-    }
-    // Get the dice roll
-    List<Integer> rolls = diceSvc.roll(count);
+        }
 
-    String csvString = rolls
-            .stream()
-            .map(v -> v.toString())
-            .collect(Collectors.joining(","));
-    return ResponseEntity.ok(csvString);
+        // Get the dice roll
+        List<Integer> rolls = diceSvc.roll(count);
+
+        // 3, 1, 5
+        String csvString = rolls
+                .stream()
+                .map(v -> v.toString())
+                .collect(Collectors.joining(","));
+
+        return ResponseEntity.ok(csvString);
     }
-    
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> getRoll(
+    public ResponseEntity<String> getRollAsJson(
             @RequestParam(name = "dice", defaultValue = "1") Integer count) {
 
         if ((count < 1) || (count > 10)) {
-            JsonObject errResp = Json.createObjectBuilder()
+            JsonObject errResp = Json
+                    .createObjectBuilder()
                     .add("error", "Valid dice count is between 1 and 10. Your count is %d".formatted(count))
                     .build();
             String payload = errResp.toString();
             // Return 400
             return ResponseEntity
-                    // .status(HttpStatus.BAD_REQUEST)
+                    //.status(HttpStatus.BAD_REQUEST) 
                     .badRequest() //400
                     .body(payload);
         }
-
-
+        
         // Get the dice roll
         List<Integer> rolls = diceSvc.roll(count);
 
         // Create the response payload
         JsonObjectBuilder builder = Json
-            .createObjectBuilder()
-            .add("count", count);
+                .createObjectBuilder()
+                .add("count", count);
 
         // Create a JsonArray
         JsonArrayBuilder arrBuilder = Json.createArrayBuilder();
         for (Integer d: rolls)
             arrBuilder.add(d);
-        // Add JsonArray (rolls) to the response
+        // Add JsonArray (rolls) to the respose
         builder = builder.add("rolls", arrBuilder);
 
-        // Get the JsonObject from JsonBuilder    
+        // Get the JsonObject object from JsonBuilder
         JsonObject resp = builder.build();
 
         return ResponseEntity.ok(resp.toString());
     }
-    
 }
